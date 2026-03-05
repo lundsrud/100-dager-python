282ab0ce914025d47862a89ae5b975351a8509b1
lundsrud <68284776+lundsrud@users.noreply.github.com>
2026-03-05 09:12:54 +0100

    Create Oppgave 1.py

---
+++ Dag 1/Oppgave 1.py
@@ -0,0 +1,7 @@
+print("Velkomme til band navn generator")
+hjemBy = input("Hva er navnet på hjembyen din ")
+print(hjemBy)
+kjealedyrNavn = input("Hva er navnet på ditt kjæredyr ")
+print(kjealedyrNavn)
+bandNavn = f"Ditt band navn er {hjemBy} {kjealedyrNavn}"
+print(bandNavn)
